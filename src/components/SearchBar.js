1fca0bd383660db4331adef3e43d09d31161401b
Bwamutala Miantezila <bwamutalamiantezila@MacBook-Pro.attlocal.net>
2020-08-20 00:47:50 -0700

    changed App from function to Object

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,36 +1,36 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-   /*  onInputChange(event){
-    console.log (event.target.value);
-    } */
+    /*  onInputChange(event){
+     console.log (event.target.value);
+     } */
 
-    state ={term: ''};
+    state = { term: '' };
 
-    onFormSubmit= event =>{
-     event.preventDefault();
-     console.log(this.state.term);
+    onFormSubmit = event => {
+        event.preventDefault();
+        this.props.onSubmit(this.state.term);
     }
 
-    render(){
+    render() {
         return (
-          <div className="ui segment">
-            {/* <form onSubmit={event => this.onFormSubmit(event)} className="ui form"> */}
-            <form onSubmit={this.onFormSubmit} className="ui form">  
-                <div className="field">
-                    <label>Image Search</label>
-                    <input 
-                     type="text" 
-                     value={this.state.term}
-                     onChange={ e=> this.setState({term: e.target.value})}                   
-                    />
+            <div className="ui segment">
+                {/* <form onSubmit={event => this.onFormSubmit(event)} className="ui form"> */}
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                    <div className="field">
+                        <label>Image Search</label>
+                        <input
+                            type="text"
+                            value={this.state.term}
+                            onChange={e => this.setState({ term: e.target.value })}
+                        />
 
-                   {/* <input type="text" onChange={this.onInputChange}/> */}
-                   {/* <input type="text" onChange={(e)=>console.log (e.target.value)}/> */}
-                </div>
-            </form>
+                        {/* <input type="text" onChange={this.onInputChange}/> */}
+                        {/* <input type="text" onChange={(e)=>console.log (e.target.value)}/> */}
+                    </div>
+                </form>
             </div>
-            );
+        );
     };
 }
 export default SearchBar;
